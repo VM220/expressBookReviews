9a4b5ba1f7aa5c13f502172efe8f5f4cbec2588a
VM220 <vmurarka.22@gmail.com>
2025-06-07 22:24:52 +0530

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,7 +26,19 @@ public_users.get("/isbn/:isbn", function (req, res) {
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const keys = Object.keys(books);
+
+  const filtered_books = keys
+    .map((key) => books[key])
+    .filter((book) => book.author === author);
+  if (filtered_books.length > 0) {
+    return res.status(300).json(filtered_books);
+  } else {
+    return res
+      .status(300)
+      .json({ message: "No books found for the given author." });
+  }
 });
 
 // Get all books based on title

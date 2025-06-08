1f97aae7b3df392efd6701c751d53ae5c7a7978d
VM220 <vmurarka.22@gmail.com>
2025-06-08 13:02:45 +0530

    Task 9 done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -54,19 +54,21 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  const isbn = parseInt(req.params.isbn);
-  const username = req.session.username;
+  const isbn = req.params.isbn;
+  const username = req.session.authorization?.username;
   const review = req.query.review || req.body.review;
-  let book = books[isbn];
-  if (!isValid(username)) {
+
+  if (!username || !isValid(username)) {
     return res
       .status(403)
       .json({ message: "You must be logged in to write a review." });
   }
+
+  let book = books[isbn];
   if (!book) {
     return res.status(404).json({ message: "Unable to find book" });
   }
+
   if (!review) {
     return res.status(400).json({ message: "Review content missing" });
   }
@@ -76,6 +78,22 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     message: `Review for book ${isbn} has been added/updated`,
     reviews: book.reviews,
   });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization?.username;
+  if (!username) {
+    return res.status(401).json({ message: "User not logged in." });
+  }
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Unable to find book." });
+  }
+  if (!books[isbn].reviews[username]) {
+    return res.status(404).json({ message: "Review by this user not found." });
+  }
+  delete books[isbn].reviews[username];
+  return res.status(200).json({ message: "Review deleted successfully." });
 });
 
 module.exports.authenticated = regd_users;
